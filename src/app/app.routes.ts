81ced24467b06fe0a3dc470cd4651e2c274748f1
newtsilv <newthonaraujo19@gmail.com>
2026-01-27 20:50:40 -0300

    feat: estrutura basica do projeto sem estilização

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomeSection } from './components/home-section/home-section';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: ``, component: HomeSection}
+];
